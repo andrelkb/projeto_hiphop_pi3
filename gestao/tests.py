876ddbb60be9626457aeec8d0f2ec4c646ccb950
ANDRELKB <andrelkb@gmail.com>
2026-05-23 17:29:32 -0300

    Implementa CI/CD no GitHub e adiciona novos testes

--- gestao/tests.py
+++ gestao/tests.py
@@ -9,3 +9,17 @@ class GestaoViewsTest(TestCase):
         """
         response = self.client.get('/alunos/')
         self.assertEqual(response.status_code, 200)
+
+    def test_pagina_chamada_carrega_com_sucesso(self):
+        """
+        Testa se a rota /chamada/ retorna o status HTTP 200 (OK).
+        """
+        response = self.client.get('/chamada/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_pagina_admin_redireciona_para_login(self):
+        """
+        Testa se a rota /admin/ protege a área e redireciona (status 302).
+        """
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
